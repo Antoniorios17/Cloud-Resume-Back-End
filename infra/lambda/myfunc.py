f244780704a2c3cfba2d283719cf836e4cce41aa
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-07-13 22:57:33 -0400

    updated terraform

---
+++ infra/lambda/myfunc.py
@@ -0,0 +1,33 @@
+import json
+import boto3
+#Library used to interact with AWS services
+
+
+
+# interacting with the dynamodb resource
+dynamodb =boto3.resource('dynamodb')
+
+# we call the dynamodb table called cloudresume-test
+table = dynamodb.Table('cloudresume-test')
+
+
+# the lambda function will get an item from the table with an id 0
+# the item with id 0 has a property called views
+
+def lambda_handler(event, context):
+    response = table.get_item(Key={
+        'id':'1'
+    })
+    # printing the number of views on the production website
+    views = response['Item']['views']
+    # we increase the number of views by one
+    # we record the viewer count
+    views = views + 1
+    print(views)
+    # we updated the table with the new value of views
+    respose = table.put_item(Item={
+        'id':'1',
+        'views':views
+    })
+
+    return views
---
+++ infra/main.tf
@@ -0,0 +1,37 @@
+resource "aws_lambda_function" "my_func" {
+  filename          = data.archive_file.zip.output_path
+  source_code_hash  = data.archive_file.zip.output_base64sha256
+  function_name     = "my_func"
+  role              = aws_iam_role.iam_for_lambda.arn
+  handler           = "func.handler"
+  runtime           = "python3.8"
+  
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "stsAssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF  
+}
+
+data "archive_file" "zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda/"
+  output_path = "${path.module}/packerlambda.zip"
+}
+
+
