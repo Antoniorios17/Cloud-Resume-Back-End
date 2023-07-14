6a5b4c4deb02e369d51e323e78c56fc6e2eef1b0
Antonio Rios <Antoniorioscarballo17@gmail.com>
2023-07-13 23:07:15 -0400

    updated terraform

--- infra/main.tf
+++ infra/main.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "iam_for_lambda" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": "stsAssumeRole",
+      "Action": "sts:AssumeRole",
       "Principal": {
         "Service": "lambda.amazonaws.com"
       },
@@ -33,5 +33,21 @@ data "archive_file" "zip" {
   source_dir  = "${path.module}/lambda/"
   output_path = "${path.module}/packerlambda.zip"
 }
+
+resource "aws_lambda_function_url" "url1" {
+  function_name       = aws_lambda_function.my_func.function_name
+  authorization_type  = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+
+}
+
 
 
